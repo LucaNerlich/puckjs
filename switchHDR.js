b350d577890670288b54bc8030f5f3b87458a998
Luca Nerlich <luca.nerlich@googlemail.com>
2023-10-04 11:35:39 +0200

    Update switchHDR.js

--- switchHDR.js
+++ switchHDR.js
@@ -1,7 +1,16 @@
-//http://www.espruino.com/BLE+Keyboard
+// http://www.espruino.com/BLE+Keyboard
 var kb = require("ble_hid_keyboard");
-NRF.setServices(undefined, { hid : kb.report });
+NRF.setServices(undefined, { hid: kb.report });
+
+// https://forum.espruino.com/conversations/390668/#comment17145677
+NRF.setAdvertising([
+  2, 1, 6,  // standard Bluetooth
+  8, 9, 80, 117, 99, 107, 46, 106, 115, // 'Puck.js' name
+  3, 3, 0x12, 0x18, // HID Service
+  3, 0x19, 0xc1, 0x03 // Appearance: Keyboard https://btprodspecificationrefs.blob.core.windows.net/assigned-numbers/Assigned%20Number%20Types/Assigned_Numbers.pdf
+]);
+
 function btnPressed() {
   kb.tap(kb.KEY.A, kb.MODIFY.LEFT_GUI, kb.MODIFY.LEFT_ALT);
 }
-setWatch(btnPressed, BTN, {edge:"rising",repeat:true,debounce:50});
+setWatch(btnPressed, BTN, { edge: "rising", repeat: true, debounce: 50 });
